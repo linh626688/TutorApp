403db6e1306cd8b4f563adc4a5f330a01f60bd2c
cakiem626688 <linh626688@gmail.com>
2016-12-06 13:21:31 +0700

    update controller , continue service

---
+++ src/main/java/helix/com/tutorapp/controller/dto/PostByTutorDTO.java
@@ -0,0 +1,53 @@
+package helix.com.tutorapp.controller.dto;
+
+/**
+ * Created by DangThanhLinh on 06/12/2016.
+ */
+public class PostByTutorDTO {
+    private Long id;
+    private String postContent;
+
+    private String subject;
+    private String time;
+    private String timePost;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getPostContent() {
+        return postContent;
+    }
+
+    public void setPostContent(String postContent) {
+        this.postContent = postContent;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public String getTimePost() {
+        return timePost;
+    }
+
+    public void setTimePost(String timePost) {
+        this.timePost = timePost;
+    }
+}
